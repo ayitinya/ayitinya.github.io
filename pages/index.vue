685cfa75161865e34e85bd9210edc8cd19b6d327
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-09-29 18:36:06 +0000

    feature: style changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@ import Typewriter from 'typewriter-effect/dist/core';
 
 const typewritterField = ref<HTMLHeadingElement | null>(null)
 const about = ref<HTMLElement | null>(null)
+const work = ref<HTMLElement | null>(null)
+const contact = ref<HTMLElement | null>(null)
+const hero = ref<HTMLElement | null>(null)
+const navbar = ref<HTMLElement | null>(null)
+
+
 
 onMounted(() => {
     const typewriter = new Typewriter(typewritterField.value, {
@@ -11,7 +17,33 @@ onMounted(() => {
         autoStart: true,
         strings: ['a web developer', 'an engineer', 'a student', 'a gamer', 'a reader', 'a writer', 'a thinker', 'a dreamer', 'a creator', 'a problem solver', 'a learner', 'a teacher', 'a friend', 'a brother', 'a son', 'a husband', 'a father', 'a human'],
     });
+
+    const observerOptions = {
+        threshold: 0.6
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navbar.value?.classList.remove('bg-gray-900')
+                navbar.value?.classList.remove('opacity-95')
+                navbar.value?.classList.remove('text-white')
+                navbar.value?.classList.remove('drop-shadow-sm')
+                navbar.value?.classList.add('bg-transparent')
+            } else {
+                navbar.value?.classList.remove('bg-transparent')
+                navbar.value?.classList.add('bg-gray-900')
+                navbar.value?.classList.add('opacity-95')
+                navbar.value?.classList.add('text-white')
+                navbar.value?.classList.add('drop-shadow-sm')
+            }
+        })
+    }, observerOptions)
+
+
+    observer.observe(hero.value)
 })
+
 
 const scrollTo = (element: HTMLElement) => {
     element.scrollIntoView({ behavior: 'smooth' });
@@ -20,29 +52,30 @@ const scrollTo = (element: HTMLElement) => {
 </script>
 
 <template>
-    <div class="pb-20">
+    <div>
 
-        <div>
+        <div id="navbar" class="sticky top-0 py-2" ref="navbar">
             <nav class="flex flex-row justify-between">
                 <span></span>
                 <ul class="flex flex-row items-end">
                     <li class="pr-2" @click="scrollTo(about)">About Me</li>
-                    <li class="pr-2">My Work</li>
-                    <li class="pr-2">Contact Me</li>
-                    <li class="pr-2">Resume</li>
+                    <li class="pr-2" @click="scrollTo(work)">My Work</li>
+                    <li class="pr-2" @click="scrollTo(contact)">Contact Me</li>
+                    <li class="pr-2"><a class="outline-black border border-solid border-black px-1"
+                            href="/images/my_image.jpg" download="rudy_ayitinya_resume">Resume</a></li>
                 </ul>
             </nav>
         </div>
 
-        <div class="h-screen flex flex-col justify-center items-center">
+        <div class="h-screen flex flex-col justify-center items-center" ref="hero">
             <h1 class="text-5xl font-bold">Hello, I am Rudy Ayitinya, <span class="font-light"
                     ref="typewritterField"></span></h1>
-            <span class="text-2xl">more</span>
+            <span class="text-2xl" @click="scrollTo(about)">More</span>
         </div>
 
 
-        <div class="flex justify-center items-center px-20">
-            <section id="about-me" ref="about">
+        <div class="flex justify-center flex-col items-center px-20">
+            <section id="about-me" class="h-screen flex flex-col justify-center " ref="about">
                 <h2 class="text-3xl">About Me</h2>
                 <div class="flex flex-row">
                     <div>
@@ -63,10 +96,53 @@ const scrollTo = (element: HTMLElement) => {
                     </div>
                 </div>
             </section>
+
+            <section id="work" class="h-screen flex flex-col justify-center " ref="work">
+                <h2 class="text-3xl">My Work</h2>
+                <div>
+                    <p>Section under development. Please do check later</p>
+                </div>
+            </section>
+
+            <section id="work" class="h-screen flex flex-col justify-center " ref="contact">
+                <h2 class="text-3xl text-center">Want to get in touch?</h2>
+                <div>
+                    <div>
+                        I am currently open for opportunities. If you have any questions, feel free to reach out to me.
+                    </div>
+                    <div class="flex justify-center pt-5">
+                        <ClientOnly>
+                            <a href="https://github.com/ayitinya" target="_blank" rel="noopener noreferrer"
+                                class="pr-3">
+                                <font-awesome-icon icon="fa-brands fa-github" class="fa-xl" />
+                            </a>
+                            <a href="https://www.linkedin.com/in/ayitinya/" target="_blank" rel="noopener noreferrer"
+                                class="pr-3">
+                                <font-awesome-icon icon="fa-brands fa-linkedin-in" class="fa-xl" />
+                            </a>
+                            <a href="https://www.twitter.com/ayitinya" target="_blank" rel="noopener noreferrer"
+                                class="pr-3">
+                                <font-awesome-icon icon="fa-brands fa-twitter" class="fa-xl" />
+                            </a>
+                            <a href="mailto:aytinya@outlook.com" class="pr-3">
+                                <font-awesome-icon icon="fa-regular fa-envelope" class="fa-xl" />
+                            </a>
+                            <template #fallback>
+                                <a href="https://github.com/ayitinya" target="_blank" rel="noopener noreferrer"
+                                    class="pb-3">Github</a>
+                                <a href="https://www.linkedin.com/in/ayitinya/" target="_blank"
+                                    rel="noopener noreferrer" class="pb-3">LinkedIn</a>
+                                <a href="https://www.twitter.com/ayitinya" class="pb-3">Twitter</a>
+                                <a href="mailto:aytinya@outlook.com" class="pr-3">Mail</a>
+                            </template>
+                        </ClientOnly>
+                    </div>
+                </div>
+            </section>
         </div>
 
 
-        <div class="fixed bottom-0 left-0 flex flex-col pl-5 pb-5">
+        <div class="fixed bottom-0 left-0 flex flex-col pl-5 pb-5 flex items-center">
             <ClientOnly>
                 <a href="https://github.com/ayitinya" target="_blank" rel="noopener noreferrer" class="pb-3">
                     <font-awesome-icon icon="fa-brands fa-github" class="fa-xl" />
@@ -89,6 +165,8 @@ const scrollTo = (element: HTMLElement) => {
                     <a href="mailto:aytinya@outlook.com" class="pb-3">Mail</a>
                 </template>
             </ClientOnly>
+            <div id="line" class="h-32 border-r border-black"></div>
         </div>
     </div>
 </template>
+
