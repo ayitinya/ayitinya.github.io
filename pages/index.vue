e019484b9b8f1d7609274e63403fab20f167e307
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-09-30 13:25:27 +0000

    style: style fixed

--- pages/index.vue
+++ pages/index.vue
@@ -67,6 +67,9 @@ onMounted(() => {
 
 const scrollTo = (element: HTMLElement) => {
     element.scrollIntoView({ behavior: 'smooth' });
+    if (!navClosed.value) {
+        navClosed.value = true
+    }
 }
 
 </script>
@@ -95,37 +98,40 @@ const scrollTo = (element: HTMLElement) => {
                 </ul>
                 <ClientOnly>
                     <span class="md:hidden">
-                        <font-awesome-icon icon="fa-solid fa-bars" @click="navClosed = false"/>
+                        <font-awesome-icon icon="fa-solid fa-bars" @click="navClosed = false" />
                     </span>
                 </ClientOnly>
 
-                <div class="bg-[#e0e0e0] w-full h-screen fixed top-0 left-0 z-50 transition-all ease-in-out duration-500" :class="{'hidden': navClosed}">
+                <div class="bg-[#e0e0e0] w-full h-screen fixed top-0 left-0 z-50 transition-all ease-in-out duration-500"
+                    :class="{'hidden': navClosed}">
                     <div class="flex justify-end py-5 pr-6">
-                        <font-awesome-icon icon="fa-solid fa-xmark" @click="navClosed = true"/>
+                        <font-awesome-icon icon="fa-solid fa-xmark" @click="navClosed = true" />
                     </div>
                     <ul class="flex flex-col justify-center items-center h-full">
-                        <li class="py-5">About Me</li>
-                        <li class="py-5">My Work</li>
-                        <li class="py-5">Contact Me</li>
+                        <li class="py-5" @click="scrollTo(about)">About Me</li>
+                        <li class="py-5" @click="scrollTo(work)">My Work</li>
+                        <li class="py-5" @click="scrollTo(contact)">Contact Me</li>
                         <li class="py-5">Resume</li>
                     </ul>
                 </div>
             </nav>
         </div>
 
-        <div class="min-h-screen flex flex-col justify-center items-center" ref="hero">
-            <h1 class="lg:text-5xl md:text-4xl text-xl font-bold">Hello, I am Rudy Ayitinya, </h1>
-            <span class="font-light lg:text-4xl md:text-3xl" ref="typewritterField"></span>
+        <div class="min-h-screen flex-col justify-center items-center flex" ref="hero">
+            <span class="md:hidden">Hello, I am</span>
+            <h1 class="text-4xl text-center font-bold md:hidden">Rudy Ayitinya </h1>
+            <h1 class="lg:text-5xl md:text-4xl text-xl font-bold hidden md:block">Hello, I am Rudy Ayitinya, </h1>
+            <span class="font-light lg:text-4xl text-3xl" ref="typewritterField"></span>
             <span @click="scrollTo(about)"
                 class="hover:scale-110 transition-all ease-in-out duration-500   cursor-pointer">More</span>
         </div>
 
 
         <div class="flex justify-center flex-col items-center md:px-20 px-5">
-            <section id="about-me" class="min-h-screen flex flex-col justify-center" ref="about">
+            <section id="about-me" class="min-h-screen flex flex-col justify-center max-w-[900px]" ref="about">
                 <h2 class="text-3xl">About Me</h2>
                 <div class="flex lg:flex-row flex-col">
-                    <div>
+                    <div class="md:pr-3">
                         I am an Electrical/Electronics Engineering undergraduate student at the Kwame Nkrumah University
                         of Science and Technology, Kumasi. I am a self-taught full-stack web developer and I am
                         passionate about
@@ -174,23 +180,23 @@ const scrollTo = (element: HTMLElement) => {
                     <div class="flex justify-center pt-5">
                         <ClientOnly>
                             <a href="https://github.com/ayitinya" target="_blank" rel="noopener noreferrer"
-                                class="pr-3">
+                                class="px-3">
                                 <font-awesome-icon icon="fa-brands fa-github" class="fa-xl" />
                             </a>
                             <a href="https://www.linkedin.com/in/ayitinya/" target="_blank" rel="noopener noreferrer"
-                                class="pr-3">
+                                class="px-3">
                                 <font-awesome-icon icon="fa-brands fa-linkedin-in" class="fa-xl" />
 
                             </a>
                             <a href="https://www.twitter.com/ayitinya" target="_blank" rel="noopener noreferrer"
-                                class="pr-3">
+                                class="px-3">
                                 <font-awesome-icon icon="fa-brands fa-twitter" class="fa-xl" />
                             </a>
                             <a href="https://stackoverflow.com/users/13605694/ayitinya" target="_blank"
                                 rel="noopener noreferrer" class="pr-3">
                                 <font-awesome-icon icon="fa-brands fa-stack-overflow" class="fa-xl" />
                             </a>
-                            <a href="mailto:aytinya@outlook.com" class="pr-3">
+                            <a href="mailto:aytinya@outlook.com" class="px-3">
                                 <font-awesome-icon icon="fa-regular fa-envelope" class="fa-xl" />
                             </a>
                             <template #fallback>
@@ -212,7 +218,7 @@ const scrollTo = (element: HTMLElement) => {
         </div>
 
 
-        <div class="fixed bottom-0 left-0 md:flex flex-col pl-5 pb-5 flex items-center hidden" ref="socialLinks">
+        <div class="fixed bottom-0 left-0 lg:flex flex-col pl-5 pb-5 flex items-center hidden" ref="socialLinks">
             <ClientOnly>
                 <a href="https://github.com/ayitinya" target="_blank" rel="noopener noreferrer" class="pb-3">
                     <font-awesome-icon icon="fa-brands fa-github" class="fa-xl" />
