98a27958b34d96a76f78601766663f65de235105
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-09-30 12:50:46 +0000

    feature: imports more icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,6 +1,6 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { fab, faGithub, faTwitter, faLinkedin, faLinkedinIn, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 // import envelop icon 
@@ -10,7 +10,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas, faGithub, faEnvelope, faTwitter, faLinkedin, faLinkedinIn, fab)
+library.add(fas, faGithub, faEnvelope, faTwitter, faLinkedin, faLinkedinIn, fab, faBars, faStackOverflow, faXmark)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
