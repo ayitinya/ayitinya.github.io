b71e5fe4274e2ba3c452adc2c52b5ebde6eac950
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-11-24 14:33:46 +0000

    chore: removed font-awesome and nuxt-tailwind
    removed font-awesome and replaced with
    nuxt-icon
    replaced nuxt-tailwind with tailwind package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/tailwindcss',
         '@nuxtjs/color-mode',
+        'nuxt-icon'
     ],
     css: [
-        '@fortawesome/fontawesome-svg-core/styles.css',
-      ],
-})
+        '~/assets/css/tailwind.css',
+    ],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+}
+)
