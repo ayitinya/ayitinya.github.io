423f2a63bc439fdbcf8ccfa4d6fd72a820e2fcac
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-09 06:40:20 +0000

    feat: added cross domain linking to gtag

--- plugins/vue-gtag.client.ts
+++ plugins/vue-gtag.client.ts
@@ -4,6 +4,11 @@ export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.use(VueGTag, {
         config: {
             id: 'G-LTMV89EKNM',
+            params: {
+                linker: {
+                    domains: ['ayitinya.me', 'blog.ayitinya.me'],
+                }
+            }
         },
     },
         nuxtApp.$router
