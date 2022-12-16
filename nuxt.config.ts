4ae96b153f40a383d84112238c67cc14617f9bf9
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-16 03:37:44 +0000

    feat: analytics works only in production now

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,17 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxtjs/color-mode',
-        'nuxt-icon'
-    ],
-    css: [
-        '~/assets/css/tailwind.css',
-    ],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-        },
+  modules: ["@nuxtjs/color-mode", "nuxt-icon"],
+  css: ["~/assets/css/tailwind.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-}
-)
+  },
+  runtimeConfig: {
+    public: {
+        ENV: process.env.VERCEL_ENV || process.env.NODE_ENV || "development",
+        G_TAG_ID: process.env.G_TAG_ID,
+    }
+  }
+});
