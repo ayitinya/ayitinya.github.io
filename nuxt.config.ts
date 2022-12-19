5ffc988ac456e39271091c302e83c0a13e667c9a
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-19 15:36:39 +0000

    fix: fix merge errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,16 +10,18 @@ export default defineNuxtConfig({
   },
   image: {
     cloudinary: {
-        baseURL: "https://res.cloudinary.com/dnha19v3n/image/upload/",
+      baseURL: "https://res.cloudinary.com/dnha19v3n/image/upload/",
+    },
+  },
   runtimeConfig: {
     public: {
-        ENV: process.env.VERCEL_ENV || process.env.NODE_ENV || "development",
-        G_TAG_ID: process.env.G_TAG_ID,
-    }
+      ENV: process.env.VERCEL_ENV || process.env.NODE_ENV || "development",
+      G_TAG_ID: process.env.G_TAG_ID,
+    },
   },
   nitro: {
     prerender: {
-      routes: ["/",],
-    }
-  }
+      routes: ["/"],
+    },
+  },
 });
