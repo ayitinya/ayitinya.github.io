5024d0fc5e0edf1807ea6c8bfd52ec86421a1342
Rudy Ayitinya Sulley <ayitinya@outlook.com>
2022-12-19 15:39:12 +0000

    merge: publish new site (#2)
    * feat: redesigns site
    
    * fix: fix merge errors

---
+++ components/Project.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+withDefaults(
+    defineProps<{
+        title: string,
+        description: string,
+        image: string,
+        imgAlt: string,
+        website?: string | null,
+        sourceCode?: string | null
+    }>(),
+    {
+        website: null,
+        sourceCode: null
+    }
+)
+</script>
+
+<template>
+    <div class="flex flex-col border">
+        <NuxtImg provider="cloudinary" quality="50"  :src="image" :alt="imgAlt" class="w-full max-h-60 object-cover hover:object-scale-down" />
+        <div class="p-4 min-h-[200px] flex flex-col mt-auto">
+            <h4 class="text-2xl font-bold">{{ title }}</h4>
+            <p class="text-lg font-thin">
+                {{ description }}
+            </p>
+            <div class="flex flex-row gap-4 mt-auto">
+                <NuxtLink v-if="website" :external="true" :to="website" target="_blank"
+                    class="bg-black text-white rounded-lg border p-2">View Website</NuxtLink>
+                <NuxtLink v-if="sourceCode" :external="true" :to="sourceCode" target="_blank"
+                    class="border border-black rounded-lg p-2">View Source</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/color-mode", "nuxt-icon"],
+  modules: ["@nuxtjs/color-mode", "nuxt-icon", "@nuxt/image-edge"],
   css: ["~/assets/css/tailwind.css"],
   postcss: {
     plugins: {
@@ -8,15 +8,20 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/dnha19v3n/image/upload/",
+    },
+  },
   runtimeConfig: {
     public: {
-        ENV: process.env.VERCEL_ENV || process.env.NODE_ENV || "development",
-        G_TAG_ID: process.env.G_TAG_ID,
-    }
+      ENV: process.env.VERCEL_ENV || process.env.NODE_ENV || "development",
+      G_TAG_ID: process.env.G_TAG_ID,
+    },
   },
   nitro: {
     prerender: {
-      routes: ["/",],
-    }
-  }
+      routes: ["/"],
+    },
+  },
 });
